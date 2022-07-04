edd1ecd6e5488fdb728cda64de62cfa550242b98
inmessi <deydebasmit22@gmail.com>
2022-07-04 16:21:26 +0530

    done

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const app = express();
 
 const port = process.env.PORT || 80;
 
+app.use('/', express.static(path.join(__dirname, 'views')));
 app.get('/', (req, res)=> {
-    res.status(200).sendFile(path.join(__dirname, 'views'));
+    res.status(200).sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(port, ()=> {
