d82947dbfd30131ce41e853ea5dae4f2a6611ad3
inmessi <deydebasmit22@gmail.com>
2022-07-09 21:53:49 +0530

    nodejs files added

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const app = express();
 
 const port = process.env.PORT || 80;
 
-app.get('/', (req, res)=> {
-    res.status(200).sendFile(path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
+// we have added views and public both to path so we can now consider them to be in this path where index.js is
+
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    var data = {
+        name: "debasmit",
+        roll: "69",
+        class: "X"
+    };
+    res.render('index', data);
 });
 
 app.listen(port, ()=> {
